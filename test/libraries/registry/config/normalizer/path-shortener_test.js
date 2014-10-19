d977d2651bd60eb526b0c7f5c1cdd7abd919f4f5
Kristian Mandrup <kmandrup@gmail.com>
2014-10-19 16:56:18 +0300

    file is normalized

--- test/libraries/registry/config/normalizer/path-shortener_test.js
+++ test/libraries/registry/config/normalizer/path-shortener_test.js
@@ -2,4 +2,31 @@
 (function(){
   var PathShortener;
   PathShortener = require('../../../../lib/registry/config/normalizer/path-shortener');
+  describe('PathShortener', function(){
+    var config;
+    config = {};
+    config.simple = {
+      main: {
+        files: ['dist/js/bootstrap.js']
+      },
+      scripts: {
+        dir: 'dist/js',
+        files: ['bootstrap.js']
+      }
+    };
+    describe('create(config)', function(){});
+    return context('instance', function(){
+      var shortener;
+      beforeEach(function(){
+        return shortener = new PathShortener(config.simple);
+      });
+      describe('shorten-paths', function(){
+        return specify('shortens all paths', function(){
+          return expect(shortener.shortener()).to.eql('bootstrap.js');
+        });
+      });
+      describe('shorten-paths-for(entry)', function(){});
+      return describe('shorten-path(file)', function(){});
+    });
+  });
 }).call(this);
--- test/libraries/registry/config/normalizer/path-shortener_test.ls
+++ test/libraries/registry/config/normalizer/path-shortener_test.ls
@@ -1,1 +1,29 @@
 PathShortener = require '../../../../lib/registry/config/normalizer/path-shortener'
+
+describe 'PathShortener' ->
+
+  config = {}
+  config.simple =
+    main:
+      files: ['dist/js/bootstrap.js']
+    scripts:
+      dir: 'dist/js'
+      files: ['bootstrap.js']
+
+  describe 'create(config)' ->
+
+  context 'instance' ->
+    var shortener
+
+    before-each ->
+      shortener := new PathShortener config.simple
+
+    describe 'shorten-paths' ->
+      specify 'shortens all paths' ->
+        expect shortener.shortener! .to.eql 'bootstrap.js'
+
+    # can be reused at at lv with files
+    describe 'shorten-paths-for(entry)' ->
+
+    describe 'shorten-path(file)' ->
+
